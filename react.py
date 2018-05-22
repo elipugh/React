407c1883864d264b20e9a36f36a868be92b6f0a9
Eli Pugh <epugh@stanford.edu>
2018-05-21 21:33:42 -0700

    Update react.py

--- react.py
+++ react.py
@@ -8,7 +8,6 @@ import copy
 import StringIO
 import numpy as np
 from scipy import stats
-from util import *
 
 #############################################################
 #                                                           #
