8e9e2bfb286088e8e3c666cf2ccab51bd0b24585
derekahuang <huangda@stanford.edu>
2018-05-21 17:49:55 -0700

    testing

--- MLP.py
+++ MLP.py
@@ -1,26 +1,24 @@
 import csv, itertools, random, math,sys
 import pandas as pd
 import numpy as np
-from sklearn.neural_network import MLPRegressor
+from sklearn.neural_network import MLPRegressor, MLPClassifier
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import normalize
-
-# TODO: Validation set
 
 # TD-IDF as features using uni and bigram
 transformer = TfidfVectorizer(ngram_range=(1,2))
 
 # Read in only relevant columns. Drop all rows with a NaN value
 print("processing data")
-df = pd.read_csv(r"/Users/derekhuang/OneDrive/College/2017-2018/CS221/React/data/data1_compiled.csv",usecols=[1, 9, 10, 11, 12, 13, 14]).dropna(axis=0)
+df = pd.read_csv(r"/Users/derekhuang/OneDrive/College/2017-2018/CS221/React/data1_compiled.csv",usecols=[1, 9, 10, 11, 12, 13, 14]).dropna(axis=0)
 
 # Drop all rows where there are 0 likes 
 df = df[df.num_likes != 0]
 
 # Allocate 20% of data to test set 
-data, test = train_test_split(df, test_size=0.2)
+data, test = train_test_split(df, test_size=0.3, shuffle=False)
 
 # extract status and reactions as s, v where status = the status and values = the labels
 s_train = data.iloc[:,[0]].T.squeeze()
@@ -31,7 +29,9 @@ v_train = v_train.div(v_train.sum(axis=0), axis=1)
 
 # Set NaN values to 0
 v_train.fillna(value=0, inplace=True)
+print(v_train)
 values = v_train.T
+print(values)
 
 # Use TD-IDF as features 
 sentences = s_train.tolist()
@@ -40,7 +40,9 @@ print("data processed")
 
 # Set softmax as the output function to mirror L1 norm
 print("training...")
-clf = MLPRegressor(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(20), random_state=1)
+layers=(100,)
+# layers=(15)
+clf = MLPRegressor(solver='adam', alpha=1e-5, hidden_layer_sizes=layers, random_state=1, early_stopping=True)
 clf.out_activation_ = 'softmax'
 clf.fit(features, values) 
 print("trained")
@@ -49,11 +51,14 @@ print("trained")
 print("testing")
 s_test = test.iloc[:,[0]].T.squeeze()
 tfidf_test = transformer.transform(s_test.tolist())
+# tfidf_test = transformer.transform(["She spent the last years of her life working two jobs to help pay for the medical expenses of her husband. Now he will have enough funds for a stem cell treatment."])
 v_test = test.iloc[:,[2,3,4,5,6]].T.squeeze()
 v_test = v_test.div(v_test.sum(axis=0), axis=1)
 v_test.fillna(value=0, inplace=True)
 values_test = v_test.T
 predictions = clf.predict(tfidf_test)
+# print(values_test)
+# print(predictions)
 print("Mean squared error: %.2f" % mean_squared_error(values_test, predictions))
 # Explained variance score: 1 is perfect prediction
 print('Variance score: %.2f' % r2_score(values_test, predictions))
--- README.md
+++ README.md
@@ -3,9 +3,9 @@ Stanford CS221 Final Project
 
 We only consider non-like reactions (haha, love...). We normalized each reaction by the number of total non-like reactions and use this as our label. 
 
-First featurizer: Used an n-gram model with TF-IDF to produce features. Playing around with some of the more important parameters, we obtained: 
-  1 hidden layer with 15 neurons, with unigram and bigram: MSE of .04 and R^2 of .3
-  2 hidden layers with 20 neurons each, with unigram and bigram: MSE of .05 and R^2 of .10
-  3 hidden layers with 20 neurons each, with unigram and bigram: MSE of .06 and R^2 of 0
-  3 hidden layers with 20 neurons each, with unigram through quad-gram: MSE of .05 and R^2 of .18
-Experimenting with various metrics, we saw no noticeable gain above using 1 hidden layer with unigram and bigram features. 
+First featurizer: Used an n-gram model with TF-IDF to produce features. MLP is using Adam optimization. Playing around with some of the more important parameters, we obtained: 
+  1 hidden layer with 15 neurons, with unigram and bigram: MSE of .06 and R^2 of .20
+  1 hidden layer with 100 neurons: MSE of .06 and R^2 of .20
+  3 hidden layers with 100, 50, 10 neurons: MSE of .06 and R^2 of .22  
+  
+Experimenting with various metrics, we saw no significant gain  above using 1 hidden layer with unigram and bigram features. 
