90e5245fc33c1b7d3aefa4d29defcff4eb0d523d
derekahuang <huangda@stanford.edu>
2018-05-26 21:53:22 -0700

    edits

--- MLP.py
+++ MLP.py
@@ -40,25 +40,34 @@ print("data processed")
 
 # Set softmax as the output function to mirror L1 norm
 print("training...")
-layers=(100,)
+layers=(100)
 # layers=(15)
-clf = MLPRegressor(solver='adam', alpha=1e-5, hidden_layer_sizes=layers, random_state=1, early_stopping=True)
-clf.out_activation_ = 'softmax'
+clf = MLPRegressor(solver='adam', alpha=1e-5, hidden_layer_sizes=layers, random_state=1, early_stopping=True, activation='logistic')
 clf.fit(features, values) 
+# clf.n_layers_ = 3
+# clf.out_activation_ = 'softmax'
 print("trained")
 
 # TESTING
 print("testing")
 s_test = test.iloc[:,[0]].T.squeeze()
 tfidf_test = transformer.transform(s_test.tolist())
-# tfidf_test = transformer.transform(["She spent the last years of her life working two jobs to help pay for the medical expenses of her husband. Now he will have enough funds for a stem cell treatment."])
+# tfidf_test = transformer.transform(["Security costs for Environmental Protection Agency Administrator Scott Pruitt tally up to nearly $3.5 million for the past year, according to figures the agency released Friday."])
 v_test = test.iloc[:,[2,3,4,5,6]].T.squeeze()
 v_test = v_test.div(v_test.sum(axis=0), axis=1)
 v_test.fillna(value=0, inplace=True)
 values_test = v_test.T
 predictions = clf.predict(tfidf_test)
-# print(values_test)
-# print(predictions)
+print(s_test)
+print(values_test)
+for elem in predictions:
+	tot = 0
+	for i in range(5):
+		tot += max(0, elem[i])
+	for i in range(5):
+		elem[i] = max(0, elem[i]) / float(tot)
+
+print(predictions)
 print("Mean squared error: %.2f" % mean_squared_error(values_test, predictions))
 # Explained variance score: 1 is perfect prediction
 print('Variance score: %.2f' % r2_score(values_test, predictions))
